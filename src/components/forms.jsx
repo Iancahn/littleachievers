ee99a281bb392482e5fa404e191ee6cb54b62a8d
Ian Cahn <iancahn@live.com>
2022-08-13 18:23:12 +0200

    Added useStates
    Added useState and styling to form component

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Forms() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    // event Handlers
+
+    const submitHandler = (e) => {
+        // e.preventDefault();
+    }
+
     return (
-        <form>
-            <label>
-                Name:
-                <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
-        </form >
+        <div className="create">
+            <h2>Add a New Blog:</h2>
+            <form onSubmit={submitHandler}>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form >
+        </div>
     )
 }
 
